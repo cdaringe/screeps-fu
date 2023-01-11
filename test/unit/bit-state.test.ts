0e48b7a4dae5ecf2957bd78f0dfba104b6dd0915
cdaringe <cdaringe@gmail.com>
2023-01-10 23:33:33 -0800

    tests: whitespace, cases

--- test/unit/bit-state.test.ts
+++ test/unit/bit-state.test.ts
@@ -4,23 +4,31 @@ import { BitState30 } from "../../src/utils/bit-state-30";
 
 test("BitState", t => {
   t.throws(() => BitState.of(0));
+
   let bs = BitState.of(1);
-  t.deepEqual(bs.toArray(), []) // 1 => []
-  t.is(JSON.stringify(bs), "[1]");
   t.is(bs.length, 0);
-  bs = BitState.of(2); // 2 => [false]
-  t.deepEqual(bs.toArray(), [false]) // 2 => [false]
+  t.is(JSON.stringify(bs), "[1]");
+  t.deepEqual(bs.toArray(), []) // 1 => []
+
+  bs = BitState.of(2);
   t.is(bs.length, 1);
   t.is(JSON.stringify(bs), "[2]");
-  t.deepEqual(bs.toArray(), [false]);
+  t.deepEqual(bs.toArray(), [false]) // 2 => [false]
+
   bs = BitState.of(3);
+  t.is(bs.length, 1);
+  t.is(JSON.stringify(bs), "[3]");
   t.deepEqual(bs.toArray(), [true]) // 3 => [true]
+
   const lotsDataTrue = [...new Array(31)].map(_ => true);
   bs = BitState.ofArray(lotsDataTrue);
-  t.deepEqual(bs.toArray(), lotsDataTrue)
+  t.is(bs.length, 31)
   t.is(JSON.stringify(bs), "[2147483647,3]");
+  t.deepEqual(bs.toArray(), lotsDataTrue)
+
   const megaData = [...new Array(64)].map(_ => true);
   bs = BitState.ofArray(megaData);
+  t.is(bs.length, 64)
   t.is(JSON.stringify(bs), "[2147483647,2147483647,31]");
   t.deepEqual(bs.toArray(), megaData);
 });
